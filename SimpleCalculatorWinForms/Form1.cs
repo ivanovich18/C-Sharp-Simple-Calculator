3ab6b5f4413c944f014c3fa42442324fa84c2647
ivanovich0808 <ivan.suralta@ctu.edu.ph>
2022-06-21 09:05:52 +0800

    Finished the implementation of the user interface

--- SimpleCalculatorWinForms/Form1.cs
+++ SimpleCalculatorWinForms/Form1.cs
@@ -26,5 +26,10 @@ namespace SimpleCalculatorWinForms
         {
             MessageBox.Show("Backspace");
         }
+
+        private void btnPeriod_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Period");
+        }
     }
 }
